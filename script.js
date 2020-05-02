50b3101404f09fce5710e82d5b7c0f0de472c93e
Shawn Beaton <beat0154@algonquinlive.com>
2020-05-02 15:31:30 -0400

    update

--- script.js
+++ script.js
@@ -3,41 +3,50 @@ var reshoot = document.getElementById("reshoot");
 var ball = document.getElementById("ball");
 var shooter = document.getElementById("shooter");
 var cupsOut = [];
-function shootX(){
-    var left = parseFloat(window.getComputedStyle(ball).getPropertyValue("left"));
-    ball.classList.remove("shootX");
-    ball.style.left = "125px";
-    ball.classList.add("shootY");
-    var shootYstr = "shootY('";
-    var shootYstr1 = left.toString();
+document.addEventListener("click", shootY);
+function shootY(){
+    document.removeEventListener("click", shootY); 
+    var top = window.getComputedStyle(ball).getPropertyValue("top");
+    ball.classList.remove("shootY");
+    ball.classList.add("shootX");
+    ball.style.top = top;
+  document.getElementById("html").setAttribute("onclick","shootX('top')");
+    var shootYstr = "shootX('";
+    var shootYstr1 = top.toString();
     var shootYstr2 = "')";
     var onclick = shootYstr.concat(shootYstr1, shootYstr2);
-    body.setAttribute("onclick", onclick);
+    document.getElementById("html").setAttribute("onclick", onclick);
+    
 }
-function shootY(valueX){
-    body.setAttribute("onclick","");
-    var x = parseFloat(valueX);
-    var top = parseFloat(window.getComputedStyle(ball).getPropertyValue("top"));
-    var topABS = Math.abs(top);
-    ball.classList.remove("shootY");
-    ball.style.left = x;
-    ball.style.top = top-325;
-    if(150<topABS && topABS<175 && 0<x && x<45){
+function shootX(valueY){
+    document.getElementById("html").setAttribute("onclick","");
+    var top = parseFloat(valueY);
+    var topABS = Math.abs(parseFloat(valueY));
+    var left = window.getComputedStyle(ball).getPropertyValue("left");
+    leftABS = parseFloat(left);
+    ball.classList.remove("shootX");
+    var newtop = top-325;
+    var newtopStr = newtop.toString();
+    var px = "px";
+    var final = newtopStr.concat(px);
+    ball.style.top = final;
+    ball.style.left = left;
+    if(150<topABS && topABS<175 && -125<leftABS && leftABS<-80){
         removeCup("1");
     }
-    if(150<topABS && topABS<175 && 95<x && x<140){
+    if(150<topABS && topABS<175 && -30<leftABS && leftABS<30){
         removeCup("2");
     }
-    if(150<topABS && topABS<175 && 210<x && x<255){
+    if(150<topABS && topABS<175 && 80<leftABS && leftABS<125){
         removeCup("3");
     }
-    if(105<topABS && topABS<140 && 45<x && x<90){
+    if(105<topABS && topABS<140 && -80<leftABS && leftABS<-35){
         removeCup("4");
     }
-    if(105<topABS && topABS<140 && 150<x && x<195){
+    if(105<topABS && topABS<140 && 25<leftABS && leftABS<70){
         removeCup("5");
     }
-    if(55<topABS && topABS<80 && 90<x && x<195){
+    if(45<topABS && topABS<80 && -35<leftABS && leftABS<35){
         removeCup("6");
     }
     setTimeout(function(){
@@ -46,7 +55,7 @@ function shootY(valueX){
         }else{
             reshoot.style.display = "block";
         }
-    },1000);
+    },500);
 }
 
 function removeCup(cup){
@@ -62,9 +71,10 @@ function removeCup(cup){
 
 function reshoot1(){
     reshoot.style.display = "none";
-    ball.classList.add("shootX");
+    ball.classList.add("shootY");
     ball.style.top = "0px";
+    ball.style.left = "0px";
     setTimeout(function(){
-        body.setAttribute("onclick","shootX()");
-    },500);
+        document.addEventListener("click", shootY); 
+    },1000);
 }
